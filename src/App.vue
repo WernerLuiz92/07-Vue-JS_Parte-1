e8d9012a2c15c22a477bd22e48e90c98b05bd37e
Werner Luiz Gottschalt <fera-luiz-gottschalt@hotmail.com>
2021-02-21 17:29:38 -0300

    Aula 2 - Data biding, atalho para o V-Bind (:) e Iteração de array com o V-For

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <h1>{{ title }}</h1>
-    <img :src="picture.url" :alt="picture.title" width="100%">
+    <ul v-for="foto in pictures" :key="foto.url">
+      <li>
+        <img :src="foto.url" :alt="foto.title" width="100%">
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -10,11 +14,29 @@ export default {
   
   data() {
     return {
-      title: 'AluraPIC',
-      picture: {
-        url: 'https://super.abril.com.br/wp-content/uploads/2016/10/super_imgnao_deixe_seu_cachorro_lamber.jpg',
-        title: 'Cachorrinho fofo'
-      }
+      title: 'Cachorrinhos fofos!',
+      pictures: [
+        {
+          url: 'https://super.abril.com.br/wp-content/uploads/2016/10/super_imgnao_deixe_seu_cachorro_lamber.jpg',
+          title: 'Cachorrinho fofo 1'
+        },
+        {
+          url: 'https://super.abril.com.br/wp-content/uploads/2018/05/filhotes-de-cachorro-alcanc3a7am-o-c3a1pice-de-fofura-com-8-semanas1.png',
+          title: 'Cachorrinho fofo 2'
+        },
+        {
+          url: 'https://img.estadao.com.br/thumbs/640/resources/jpg/4/9/1580310654894.jpg',
+          title: 'Cachorrinho fofo 3'
+        },
+        {
+          url: 'https://www.petz.com.br/blog/wp-content/uploads/2020/06/animais-com-sindrome-de-down.jpg',
+          title: 'Cachorrinho fofo 4'
+        },
+        {
+          url: 'https://i.pinimg.com/originals/69/be/90/69be90cb0bdfd8285929b64d5027a9c2.jpg',
+          title: 'Cachorrinho fofo 5'
+        }
+      ]
     }
   }
 }
