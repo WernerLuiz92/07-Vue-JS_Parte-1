3b7ec931cfc6442ef75a6e3c8fa819c497cbc1a6
Werner Luiz Gottschalt <werner@lumiun.com>
2021-06-01 09:33:54 -0300

    feat: criado filtro de imagens pelo titulo

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="body">
         <h1 class="page-title">{{ title }}</h1>
+        <!--                 @input === v-on:input                 -->
+        <input type="search" @input="searchFilter = $event.target.value" class="search-filter" name="search-filter" placeholder="Buscar fotos">
         <ul class="picture-list">
-            <li class="picture-list-item" v-for="foto in pictures" :key="foto.url">
+            <li class="picture-list-item" v-for="picture in filteredPictures" :key="picture.url">
 
-                <my-panel :title="foto.title">
-                    <div slot="content">
-                        <img class="panel-image" :src="foto.url" :alt="foto.title">
-                    </div>
+                <my-panel :title="picture.title">
+                    <responsive-image slot="panel-content" :url="picture.url" :title="picture.title" />
                 </my-panel>
 
             </li>
@@ -17,16 +17,32 @@
 
 <script>
     import Panel from './components/shared/panel/Panel.vue';
+    import ResponsiveImage from './components/shared/images/ResponsiveImage.vue';
     export default {
 
         components: {
-            'my-panel': Panel
+            'my-panel': Panel,
+            'responsive-image': ResponsiveImage
         },
         data() {
             return {
                 title: 'AluraPic - Werner',
-                pictures: []
+                pictures: [],
+                searchFilter: ''
             }
+        },
+        computed: {
+
+            filteredPictures() {
+                if (this.searchFilter) {
+                    let exp = new RegExp(this.searchFilter.trim(), 'i');
+
+                    return this.pictures.filter(picture => exp.test(picture.title));
+                }
+                
+                return this.pictures;
+            }
+
         },
         created() {
 
@@ -61,8 +77,13 @@
         margin-bottom: 20px;
     }
 
-
-    .panel-image {
+    .search-filter {
+        display: block;
         width: 100%;
+        font-size: 1.5rem;
+        /* superior | direita | inferior | esquerda */
+        padding: 10px 15px 10px 15px;
+        border: solid 1px grey;
+        border-radius: 8px;
     }
 </style>
